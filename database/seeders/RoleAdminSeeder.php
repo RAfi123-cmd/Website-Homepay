8e9de63c1c3fbfab614281ae2d324d8da7b913de
Rafi123-CMD <rafiulhuda27@gmail.com>
2026-05-17 22:06:59 +0700

    install spatie and role seeder

---
+++ database/seeders/RoleAdminSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $adminRole = Role::create([
+            'name' => 'admin'
+        ]);
+        $lenderRole = Role::create([
+            'name' => 'lender'
+        ]);
+        $agentRole = Role::create([
+            'name' => 'agent'
+        ]);
+        $customerRole = Role::create([
+            'name' => 'customer'
+        ]);
+
+        $user = User::create([
+            'name' => 'Team Tedja',
+            'email' => 'team@tedja.com',
+            'phone' => '082120202825',
+            'photo' => 'rafi.png',
+            'password' => bcrypt('123123123')
+        ]);
+
+        $user->assignRole($adminRole);
+    }
+}
